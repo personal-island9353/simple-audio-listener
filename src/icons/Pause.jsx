28dd3c3acb2f0a37bbb920effc99e77d1977673d
personal.island9353 <personal.island9353@gmail.com>
2026-03-22 09:31:17 +0100

    setup React

---
+++ src/icons/Pause.jsx
@@ -0,0 +1,15 @@
+function Pause() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path fill="currentColor" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+    </svg>
+  );
+}
+
+export default Pause;
---
+++ src/icons/Play.jsx
@@ -0,0 +1,15 @@
+function Play() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path fill="currentColor" d="M8 5v14l11-7z" />
+    </svg>
+  );
+}
+
+export default Play;
